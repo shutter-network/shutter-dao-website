fd48d3a117091150129131f7123f98427c71c3d8
Daniel Dimitrov <daniel@compojoom.com>
2023-12-18 16:24:18 +0100

    fix: 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
 import { LayoutDark } from "../features/common/components/layout-dark";
-import { MerkleDropHero } from "../features/merkle-drop/components/hero";
-import { MerkleDropIntroduction } from "../features/merkle-drop/components/introduction";
-import { MerkleDropFaqs } from "../features/merkle-drop/components/faqs";
 
 export default function MerkleDrop() {
   return (
     <LayoutDark>
-      <divi>The page you are looking for was not found.</divi>
+      <div>The page you are looking for was not found.</div>
     </LayoutDark>
   );
 }
