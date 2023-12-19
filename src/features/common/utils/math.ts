8361b61cc5b143c55f1b27fcb9242c0475340f8d
Daniel Dimitrov <daniel@compojoom.com>
2023-12-19 22:09:31 +0100

    improve decimal places display in app
    The tokens that the website is going to handle has 18 decimals. No need for this to be configurable

--- src/features/common/utils/math.ts
+++ src/features/common/utils/math.ts
@@ -10,28 +10,33 @@ import { getTokenSymbol } from "./token";
 export function parseTokenAmount(amount: bigint | number | string): string {
   const web3 = new Web3();
   const bigNumberBalance = web3.utils.toBigInt(amount);
-  const decimals = process.env.REACT_APP_TOKEN_DECIMALS || 18;
+  const decimals = 18;
   const divisor = web3.utils.toBigInt("10") ** web3.utils.toBigInt(decimals);
 
-  const decimalPlaces = process.env.REACT_APP_SHOW_DECIMALS
-    ? parseInt(process.env.REACT_APP_SHOW_DECIMALS)
-    : 2; // or however you get this value
-
   const scaleFactor =
-    web3.utils.toBigInt("10") ** web3.utils.toBigInt(decimalPlaces);
+    web3.utils.toBigInt("10") ** web3.utils.toBigInt(decimals);
 
   const scaledBalance = (bigNumberBalance * scaleFactor) / divisor;
   const wholePart = (scaledBalance / scaleFactor).toString();
   const decimalPart = (scaledBalance % scaleFactor)
     .toString()
-    .padStart(decimalPlaces, "0");
+    .padStart(decimals, "0");
   const formattedBalance = `${wholePart}.${decimalPart}`;
 
   return formattedBalance;
 }
 
-export function formatTokenAmount(amount:string, suffix:string = getTokenSymbol()): string {
-  return Number(amount).toLocaleString('en-US', {maximumFractionDigits:2}) + ' ' + suffix;
+export function formatTokenAmount(
+  amount: string,
+  suffix: string = getTokenSymbol()
+): string {
+  return (
+    Number(amount).toLocaleString("en-US", {
+      maximumFractionDigits: Number(amount) < 1 ? 18 : 4,
+    }) +
+    " " +
+    suffix
+  );
 }
 
 export function convertToWei(amount: number | string) {
@@ -39,10 +44,10 @@ export function convertToWei(amount: number | string) {
   let amountStr = amount.toString();
 
   // Step 2: Split the string into whole and fractional parts
-  let [wholePart, fractionalPart = ''] = amountStr.split('.');
+  let [wholePart, fractionalPart = ""] = amountStr.split(".");
 
   // Step 3: Pad the fractional part with zeros
-  fractionalPart = fractionalPart.padEnd(18, '0');
+  fractionalPart = fractionalPart.padEnd(18, "0");
 
   // Step 4: Concatenate the whole part, the fractional part, and the necessary number of zeros
   let weiStr = wholePart + fractionalPart;
